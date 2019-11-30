1922c3d1d7220f85fcc36af47849d77067dff541
Hanjunqiang <junqiang_han@163.com>
2019-11-30 14:06:24 +0800

    [shell] add some scripts for shell.

--- shell/doc.md
+++ shell/doc.md
@@ -2,4 +2,12 @@
 # doc
 
 
-# w
+# delete array
+unset
+
+```shell
+declare -A colors
+colors["red"]="#ff0000"
+colors["green"]="#00ff00"
+colors["blue"]="#0000ff"
+```
--- shell/ul-declare.sh
+++ shell/ul-declare.sh
@@ -2,9 +2,10 @@
 # ul-declare: demonstrate case conversion via declare
 declare -u upper
 declare -l lower
+
 if [[ $1 ]]; then
-upper="$1"
-lower="$1"
-echo $upper
-echo $lower
+	upper="$1"
+	lower="$1"
+	echo $upper
+	echo $lower
 fi
