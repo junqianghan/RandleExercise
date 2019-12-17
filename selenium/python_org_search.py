57acb4bb12c36ce9fa4b381e863d514548028fde
Hanjunqiang <junqiang_han@163.com>
2019-12-17 09:13:19 +0800

    [selenium] add content in selenium

---
+++ selenium/python_org_search.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--incognito')
+driver = webdriver.Chrome(chrome_options=chrome_options)
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element_by_name("q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+print(driver.page_source)
+driver.close()
+
